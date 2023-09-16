1ee2974e6abb0e1937fb2ef97b99a52fa2dfd722
Anirudh Kaushik <>
2023-09-16 05:50:14 +0530

    discriminator done

--- discriminator.py
+++ discriminator.py
@@ -5,14 +5,43 @@ import torch.nn.functional as F
 import numpy as np
 import PIL
 
+class Block(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(Block, self).__init__()
+        self.conv = nn.Conv2d(in_ch, out_ch, 5, padding='same')
+        self.bnorm = nn.BatchNorm2d(out_ch)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.relu = nn.LeakyReLU(0.2)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bnorm(x)
+        x = self.relu(x)
+
+        x = self.pool(x)
+
+        return x
+
 class Discriminator(nn.Module):
     def __init__(self, img_channels=1):
         super(Discriminator, self).__init__()
         self.img_channels = img_channels
 
-        self.conv1 = nn.Conv2d(self.img_channels, 64, 5, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, 5, padding=1)
-        self.conv3 = nn.Conv2d(128, 256, 5, padding=1)
-        self.conv3 = nn.Conv2d(256, 512, 5, padding=1)
+        self.conv1 = Block(img_channels, 64)
+        self.conv2 = Block(64, 128)
+        self.conv3 = Block(128, 256)
+        self.conv4 = Block(256, 512)
+        
 
         self.fc = nn.Linear(512, 1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+
+        x = nn.Flatten()(x)
+        x = self.fc(x)
+        
+        return x
