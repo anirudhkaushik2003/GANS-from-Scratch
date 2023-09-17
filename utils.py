b429a2271c130ba87719be38e839fb0a7422668d
Anirudh Kaushik <>
2023-09-17 05:39:35 +0530

    Generator complete

---
+++ utils.py
@@ -0,0 +1,14 @@
+# custom weights initialization called on ``netG`` and ``netD``
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import PIL
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
